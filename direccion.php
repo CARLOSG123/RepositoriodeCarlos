9614a69bbd52c3753539a815965754a4da17c6be
CíborgAscend <lizz_gonzalez@hotmail.com>
2015-03-20 21:24:03 -0600

    micommit

---
+++ direccion.php
@@ -0,0 +1,18 @@
+﻿<?php
+$comentario;
+$calle;
+$colonia;
+$numero;
+$municipio;
+$departamento;
+
+{
+print $comentario = 'La direccion de mi casa es:';
+print $calla = 'Calle 2,';
+print $cononia= ' Colonia El Zungano,';
+print $numero= 'Casa nª 453,';
+print $municipio= 'Zacatecoluca,';
+print $departamento= ' Departamento La Paz,';
+
+}
+?>
